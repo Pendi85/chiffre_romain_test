bd0d1058957068a5b6fa8c02f153703b4fa07829
Pendi85 <81863481+Pendi85@users.noreply.github.com>
2023-02-16 14:04:38 +0100

    Test Convertir 8 en VIII

--- app/NombreRomain.ts
+++ app/NombreRomain.ts
@@ -2,7 +2,7 @@ export class NombreRomain
 {
     static convertirChiffreRomain(chiffreRomain: number) : string
     {
-        let romains : string[] = ["I", "II", "III", "IV", "V", "VI", "VII"];
+        let romains : string[] = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII"];
 
         return romains[chiffreRomain - 1];
     }
--- tests/NombreRomainTest.ts
+++ tests/NombreRomainTest.ts
@@ -78,4 +78,15 @@ describe("Test Chiffres Romain", function () {
         // ALORS le nombre romain est VII
         assert.equal(chiffreRomain, "VII");
     });
+
+    it("ETANT donne le chiffre 8 QUAND on convertit le chiffre en nombre romain ALORS le nombre est VIII", function () {
+        // Etant donne le chiffre 8
+        let chiffreArabe : number = 8;
+
+        // Quand on covertit le chiffre en nombre romain
+        let chiffreRomain : string = NombreRomain.convertirChiffreRomain(chiffreArabe);
+
+        // ALORS le nombre romain est VIII
+        assert.equal(chiffreRomain, "VIII");
+    });
 });
