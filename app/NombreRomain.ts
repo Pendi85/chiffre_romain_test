e50767f239f787fb0c4e2596c63e62ff4b75ff03
Pendi85 <81863481+Pendi85@users.noreply.github.com>
2023-02-16 14:01:42 +0100

    Test Convertir 4 en IV

--- app/NombreRomain.ts
+++ app/NombreRomain.ts
@@ -2,7 +2,7 @@ export class NombreRomain
 {
     static convertirChiffreRomain(chiffreRomain: number) : string
     {
-        let romains : string[] = ["I", "II", "III"];
+        let romains : string[] = ["I", "II", "III", "IV"];
 
         return romains[chiffreRomain - 1];
     }
