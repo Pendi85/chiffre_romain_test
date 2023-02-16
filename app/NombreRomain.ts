754e57782eca8d2ec26c418fb674f40ae070857b
Pendi85 <81863481+Pendi85@users.noreply.github.com>
2023-02-16 14:05:23 +0100

    Test Convertir 9 en IX

--- app/NombreRomain.ts
+++ app/NombreRomain.ts
@@ -2,7 +2,7 @@ export class NombreRomain
 {
     static convertirChiffreRomain(chiffreRomain: number) : string
     {
-        let romains : string[] = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII"];
+        let romains : string[] = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
 
         return romains[chiffreRomain - 1];
     }
