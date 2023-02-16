c2ab6df5253aecb76eae41c772256e7e0b475433
Pendi85 <81863481+Pendi85@users.noreply.github.com>
2023-02-16 13:57:47 +0100

    Test Convertir 2 en II

--- app/NombreRomain.ts
+++ app/NombreRomain.ts
@@ -2,7 +2,7 @@ export class NombreRomain
 {
     static convertirChiffreRomain(chiffreRomain: number) : string
     {
-        let romains : string[] = ["I"];
+        let romains : string[] = ["I", "II"];
 
         return romains[chiffreRomain - 1];
     }
